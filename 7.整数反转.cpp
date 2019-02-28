b5d71027aee17424102b2ddc986250b89d8a5360
LiuQiang <thorneliu@gmail.com>
2019-02-28 11:14:08 +0800

    roteate integer and palindrome-number

---
+++ 7.整数反转.cpp
@@ -0,0 +1,64 @@
+/*
+ * @lc app=leetcode.cn id=7 lang=cpp
+ *
+ * [7] 整数反转
+ *
+ * https://leetcode-cn.com/problems/reverse-integer/description/
+ *
+ * algorithms
+ * Easy (31.52%)
+ * Total Accepted:    83.1K
+ * Total Submissions: 263.2K
+ * Testcase Example:  '123'
+ *
+ * 给出一个 32 位的有符号整数，你需要将这个整数中每位上的数字进行反转。
+ * 
+ * 示例 1:
+ * 
+ * 输入: 123
+ * 输出: 321
+ * 
+ * 
+ * 示例 2:
+ * 
+ * 输入: -123
+ * 输出: -321
+ * 
+ * 
+ * 示例 3:
+ * 
+ * 输入: 120
+ * 输出: 21
+ * 
+ * 
+ * 注意:
+ * 
+ * 假设我们的环境只能存储得下 32 位的有符号整数，则其数值范围为 [−231,  231 − 1]。请根据这个假设，如果反转后整数溢出那么就返回 0。
+ * 
+ */
+class Solution {
+public:
+    int reverse(int x) {
+        bool negetive = false;
+        long X = x;
+        if (x < 0)
+        {
+            negetive = true;
+            X = -1 * (long)x;
+        }
+
+        long res = 0;
+        while(X)
+        {
+            auto digit = X % 10;
+            long newres = res * 10 + digit;
+            if (newres > INT_MAX) return 0; // overflow
+            
+            res = newres;
+            X = X / 10;
+        }
+
+        return negetive ? (-1 * res) : res;
+    }
+};
+
